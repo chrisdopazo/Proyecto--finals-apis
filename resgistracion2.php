cdd47e2ce0b22b9711c2722d8bacac690457afaf
CHRISTIAN NAHUEL DOPAZO <42301638@ort>
2017-06-23 11:21:24 -0300

    mlahia}

--- resgistracion2.php
+++ resgistracion2.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(E_ERROR | E_PARSE);
-$sql_query = "INSERT INTO artistas (nombre, apellido , email , password) VALUES ('$nombre', '$apellido', '$email', '$password');";
+$sql_query = "INSERT INTO usuarios (nombre, apellido , email , password) VALUES ('$nombre', '$apellido', '$email', '$password');";
 //$sql_query = "INSERT INTO artistas (Usuario, PrimerNombre, Apellido, Sexo, Password, Mail, FechaDeNacimiento, CiudadDeResidencia, PaisDeResidencia) VALUES ('joaco', 'joaquin', 'borovich', 'masculino', 'micaieru', 'joaco@joaco.com', '30/03/00', 'CABA', 'Arg');";
 if (mysqli_query($con, $sql_query))
 {
