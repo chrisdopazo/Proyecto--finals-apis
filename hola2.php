f4b7af52f5382a43a953742aeb52cc2962855e33
CHRISTIAN NAHUEL DOPAZO <42301638@ort>
2017-06-23 11:29:44 -0300

    si

---
+++ hola2.php
@@ -0,0 +1,24 @@
+    if($_SERVER['REQUEST_METHOD']=='POST'){
+
+include 'conn.php';
+
+ $con = mysqli_connect($HostName,$HostUser,$HostPass,$DatabaseName);
+ $nombre = $_POST['nombre'];
+ $apellido = $_POST['apellido'];
+ $email = $_POST['email'];
+ $password = $_POST['password'];
+	
+	$sql_query = "INSERT INTO usuarios (Usuario, PrimerNombre, Apellido, Sexo) VALUES ('$usuario', '$nombre', '$apellido', '$sexo');";
+  
+    mysqli_query($con, $sql_query);
+    $sql_query = "SELECT Usuario from usuarios where Mail = '$mail';";
+    $result = mysqli_query($con, $sql_query);
+    if (mysqli_num_rows($result)>0)
+    {
+      echo "Registración exitosa. ";
+    }
+    else
+    {
+      echo "Error en la registración. ";
+    }
+    }
