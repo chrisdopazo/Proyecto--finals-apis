352ed11d39e3275a4171e5c0445e73ee8c9e4bf4
CHRISTIAN NAHUEL DOPAZO <42301638@ort>
2017-06-23 11:18:32 -0300

    fsadfsd

---
+++ resgistracion2.php
@@ -0,0 +1,15 @@
+<?php
+error_reporting(E_ERROR | E_PARSE);
+$sql_query = "INSERT INTO artistas (nombre, apellido , email , password) VALUES ('$nombre', '$apellido', '$email', '$password');";
+//$sql_query = "INSERT INTO artistas (Usuario, PrimerNombre, Apellido, Sexo, Password, Mail, FechaDeNacimiento, CiudadDeResidencia, PaisDeResidencia) VALUES ('joaco', 'joaquin', 'borovich', 'masculino', 'micaieru', 'joaco@joaco.com', '30/03/00', 'CABA', 'Arg');";
+if (mysqli_query($con, $sql_query))
+{
+  $mensajes += "Bienvenido!";
+}
+/*
+else
+{
+ $mensajes += mysqli_error($con) . "/";
+}
+*/
+?>
