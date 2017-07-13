cb6af1d78d361e93d2b85ff0424c7367f2874b19
Christian <chrisdopazo2000@gmail.com>
2017-07-13 16:49:56 -0300

    error tonto

--- traersalas.php
+++ traersalas.php
@@ -3,9 +3,9 @@ include 'conn.php';
 
 $conn = new mysqli($HostName, $HostUser, $HostPass, $DatabaseName);
 
-    $id = $_GET["id"];
-	$query = "Select * from sala where id between ($id+1) and ($id+4)";
-	$result = mysqli_query($connection,$query);
+    $id = $_POST["id"];
+	$query = "Select * from sala where id between ($id+1) and ($id+3)";
+	$result = mysqli_query($conn,$query);
 	while ($row = mysqli_fetch_assoc($result)) {
 			
 		$array[] = $row;	
